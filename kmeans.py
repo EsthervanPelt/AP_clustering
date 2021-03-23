f6b65bd8a3a8a41b436e7f89a58b06ed3493d0c2
EsthervanPelt <71495755+EsthervanPelt@users.noreply.github.com>
2021-03-23 11:32:07 +0100

    construct graph from data

--- kmeans.py
+++ kmeans.py
@@ -6,6 +6,7 @@ Created on Mon Mar 22 12:15:42 2021
 """
 import random as rd
 from util import euclidean
+from util import silhouette_coeff
 
 def initial_clustering(k: int, data: dict):
     clusters = {}
@@ -57,4 +58,47 @@ def assign_datapoints(data: dict, clusters: dict, dist = 0): # 0 is euclidean, 1
         
     return data, clusters
         
+def clustering(data: dict, k: int, max_iterations: int, dist = 0):
+    # choose random centroids and assign datapoints
+    clusters = initial_clustering(k, data);
+    data, clusters = assign_datapoints(data, clusters, dist = 0)
+    
+    S = [0]
+    # # iterate 
+    max_iterations = 20
+    
+    for i in range(max_iterations):
+        data, clusters = assign_datapoints(data, clusters, dist = 0)
         
+        non_zero_clusters = {}
+        for key in clusters:
+            if clusters[key][1] != []: non_zero_clusters[key] = clusters[key] 
+        
+        clusters = compute_centroid(data, non_zero_clusters)
+        
+        S.append(silhouette_coeff(data, clusters, dist = 0))
+    
+        if S[-1] == S[-2]: break
+     
+    print('Silhouette coefficient:', S[-1], '\n')
+    
+    for key in clusters:
+        
+        NB = 0
+        BRCA = 0
+        KIRC = 0
+        COAD_READ = 0
+        
+        for datapoint in clusters[key][1]:
+            if data[datapoint][1] == 'NB': NB += 1
+            if data[datapoint][1] == 'BRCA': BRCA += 1
+            if data[datapoint][1] == 'KIRC': KIRC += 1
+            if data[datapoint][1] == 'COAD/READ': COAD_READ += 1
+            
+        print('Cluster', key, '\n',
+              'NB:       ', NB, '\n', 
+              'BRCA:     ', BRCA, '\n',
+              'KIRC:     ', KIRC, '\n',
+              'COAD/READ:', COAD_READ)
+    
+    return data, clusters, S[-1]
